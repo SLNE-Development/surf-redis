af24d71c52a0585146c793dca85a65e1ce4195a8
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-12-21 15:45:11 +0100

    chore: build.gradle.kts and .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,17 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.serialization") version "1.9.22"
-    `maven-publish`
+    id("dev.slne.surf.surfapi.gradle.core")
 }
 
-group = "de.slne"
-version = "1.0.0"
+group = "dev.slne.surf.redis"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    // Lettuce Redis client
     implementation("io.lettuce:lettuce-core:6.3.0.RELEASE")
-    
-    // Logging
-    implementation("org.slf4j:slf4j-api:2.0.9")
-    
-    // Kotlin
-    implementation(kotlin("stdlib"))
-    implementation(kotlin("reflect"))
-    
-    // Kotlin Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-    
-    // Kotlin Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.3")
-    
-    // Fastutil for high-performance collections
-    implementation("it.unimi.dsi:fastutil:8.5.12")
-    
-    // Test dependencies
+
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
@@ -40,8 +19,4 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(17)
 }
