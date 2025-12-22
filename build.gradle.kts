b31e5c694795fdeb7883e76a625a137689636fa4
twisti <denizgebauer@web.de>
2025-12-22 22:51:02 +0100

    Add Maven publishing support and GitHub Actions for release automation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import dev.slne.surf.surfapi.gradle.util.slneReleases
+
 plugins {
     id("dev.slne.surf.surfapi.gradle.core") version "1.21.11+"
 //    id("dev.slne.surf.surfapi.gradle.standalone") version "1.21.11+" /* Uncomment to use tests */
@@ -16,6 +18,12 @@ dependencies {
     testImplementation("com.redis:testcontainers-redis:2.2.4")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+}
+
+publishing {
+    repositories {
+        slneReleases()
+    }
 }
 
 tasks.test {
