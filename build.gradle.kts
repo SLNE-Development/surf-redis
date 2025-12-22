70279eb58fb55e5db8d97f264bc71bb115443469
twisti <denizgebauer@web.de>
2025-12-22 10:15:19 +0100

    Refactored build scripts: migrated plugins, updated dependencies, adjusted repository settings, and downgraded Gradle wrapper version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,23 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.serialization") version "1.9.22"
-    `maven-publish`
+    id("dev.slne.surf.surfapi.gradle.core") version "1.21.11+"
 }
 
-group = "de.slne"
-version = "1.0.0"
+group = "dev.slne"
+version = findProperty("version") as String
 
-repositories {
-    mavenCentral()
-}
 
 dependencies {
     // Lettuce Redis client
-    implementation("io.lettuce:lettuce-core:6.3.0.RELEASE")
-    
-    // Logging
-    implementation("org.slf4j:slf4j-api:2.0.9")
-    
-    // Kotlin
-    implementation(kotlin("stdlib"))
-    implementation(kotlin("reflect"))
-    
-    // Kotlin Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-    
-    // Kotlin Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.3")
-    
-    // Fastutil for high-performance collections
-    implementation("it.unimi.dsi:fastutil:8.5.12")
-    
+    implementation("io.lettuce:lettuce-core:7.2.1.RELEASE")
+
+
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.3")
+
+
     // Test dependencies
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
 }
 
 tasks.test {
@@ -43,5 +25,5 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.slne.dev/repository/maven-public/") { name = "maven-public" }
+    }
+}
+
 rootProject.name = "surf-redis"
