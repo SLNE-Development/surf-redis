5d749fbea39b8fa921858b4bc14fcf5c724ac83c
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-05-26 02:01:06 +0200

    feat: refactor StandaloneRedisInstance to improve instance management and encapsulate configuration

--- surf-redis-standalone/src/main/kotlin/dev/slne/surf/redis/StandaloneRedisInstance.kt
+++ surf-redis-standalone/src/main/kotlin/dev/slne/surf/redis/StandaloneRedisInstance.kt
@@ -3,19 +3,33 @@ package dev.slne.surf.redis
 import com.google.auto.service.AutoService
 import java.nio.file.Path
 
-@AutoService(RedisInstance::class)
 class StandaloneRedisInstance(
     val name: String,
-    configPath: Path,
-) : RedisInstance() {
-    override val dataPath = configPath
-    override fun tryExtractPluginNameFromClass(clazz: Class<*>) = name
+    private val configPath: Path,
+) {
 
     fun create() {
-        load()
+        InstanceImpl.pluginName = name
+        InstanceImpl.pluginDataPath = configPath
+        RedisInstance.instance.load()
     }
 
     fun shutdown() {
-        disable()
+        RedisInstance.instance.disable()
+    }
+
+    @AutoService(RedisInstance::class)
+    class InstanceImpl : RedisInstance() {
+
+        companion object {
+            lateinit var pluginName: String
+            lateinit var pluginDataPath: Path
+        }
+
+        override val dataPath: Path
+            get() = pluginDataPath
+
+        override fun tryExtractPluginNameFromClass(clazz: Class<*>) =
+            pluginName
     }
 }
