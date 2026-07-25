4c668fd27a99ea012f8183efa606ef28c762e86c
twisti-dev <76837088+twisti-dev@users.noreply.github.com>
2026-07-25 16:32:58 +0200

    ✨ feat(config): add support for environment variables in Redis configuration
    - introduce RedisEnvironment object to manage environment variables
    - allow overriding config.yml values with environment variables
    - update RedisConfig to use environment variables for host, port, password, and client name

--- surf-redis-core/src/main/kotlin/dev/slne/surf/redis/credentials/RedisCredentialsProviderImpl.kt
+++ surf-redis-core/src/main/kotlin/dev/slne/surf/redis/credentials/RedisCredentialsProviderImpl.kt
@@ -1,7 +1,7 @@
 package dev.slne.surf.redis.credentials
 
 import com.google.auto.service.AutoService
-import dev.slne.surf.redis.config.RedisConfig
+import dev.slne.surf.redis.config.redisConfig
 import org.redisson.misc.RedisURI
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
@@ -9,18 +9,17 @@ import java.nio.charset.StandardCharsets
 @AutoService(RedisCredentialsProvider::class)
 class RedisCredentialsProviderImpl : RedisCredentialsProvider {
     override fun redisURI(): RedisURI {
-        val config = RedisConfig.getConfig()
         val redisURIString = buildString {
             append(RedisURI.REDIS_PROTOCOL)
-            val password = config.password
+            val password = redisConfig.password
             if (!password.isNullOrEmpty()) {
                 append(URLEncoder.encode(password, StandardCharsets.UTF_8))
                 append('@')
             }
 
-            append(config.host)
+            append(redisConfig.host)
             append(':')
-            append(config.port)
+            append(redisConfig.port)
         }
 
         return RedisURI(redisURIString)
